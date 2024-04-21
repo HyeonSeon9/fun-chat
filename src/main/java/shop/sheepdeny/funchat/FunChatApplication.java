21d5aaff84d7b1600d9e340205e36b428b5ebdf7
HyeonSeon9 <a01062704312@gmail.com>
2024-04-21 21:45:00 +0900

    Feat: 초기화면 수정, 닉네임 입력 후 접속시 화면에 표시

--- src/main/java/shop/sheepdeny/funchat/FunChatApplication.java
+++ src/main/java/shop/sheepdeny/funchat/FunChatApplication.java
@@ -3,6 +3,8 @@ package shop.sheepdeny.funchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.http.Cookie;
+
 @SpringBootApplication
 public class FunChatApplication {
 
---
+++ src/main/java/shop/sheepdeny/funchat/config/WebSocketConfig.java
@@ -0,0 +1,44 @@
+package shop.sheepdeny.funchat.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/chat").withSockJS().setInterceptors(new HttpSessionHandshakeInterceptor() {
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                log.info("New connection from: {}", request.getRemoteAddress().getHostName());
+                return super.beforeHandshake(request, response, wsHandler, attributes);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        log.info("broker");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
